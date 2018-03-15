ce4f9d131a756edde492ff405a45a9d43f407794
sylvie chassery <sylviechassery@gmail.com>
2018-03-15 13:49:03 +0100

    jKDLM%QD2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // Modification effectuées dans Git
-            // Modification effectuées dans Visual
+            // Modification effectuées dans VisualK/
         }
     }
 }
